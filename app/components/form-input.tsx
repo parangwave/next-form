e5d687acf1f7d0422834f029b54ca8e3dc74e2bf
Oh Jisu <parangwavetraveller@gmail.com>
2024-08-07 00:08:32 +0900

    feat: Add form-input component

---
+++ app/components/form-input.tsx
@@ -0,0 +1,34 @@
+interface IFormProps {
+  type: string;
+  placeholder: string;
+  required: boolean;
+  errors: string[];
+  name: string;
+}
+
+export default function FormInput({
+  type,
+  placeholder,
+  required,
+  errors,
+  name,
+}: IFormProps) {
+  return (
+    <div>
+      <input
+        name={name}
+        className="h-10 p-2 w-full rounded-full border-neutral-100 bg-transparent ring-1 ring-neutral-200 transition-colors duration-100 placeholder:text-neutral-400 focus:outline-none focus:ring-2 focus:ring-neutral-300 ring-offset-2"
+        type={type}
+        placeholder={placeholder}
+        required={required}
+      />
+      <div className="my-2">
+        {errors.map((error, index) => (
+          <span key={index} className="font-medium text-red-500 inline-block">
+            {error}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+}

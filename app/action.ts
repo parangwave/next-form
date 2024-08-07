dd9aedfb79d6de01354bd2cb961c62386c349d6f
Oh Jisu <parangwavetraveller@gmail.com>
2024-08-08 02:12:15 +0900

    feat: Add zod schema
    input 입력값 검증을 위한 zod schema 생성

--- app/action.ts
+++ app/action.ts
@@ -1,5 +1,19 @@
 "use server";
 
+import { z } from "zod";
+
+const schema = z.object({
+  email: z
+    .string()
+    .email()
+    .regex(/@zod\.com$/, "Only @zod.com emails are allowed"),
+  username: z.string().min(5, "Username should be at least 5 characters long"),
+  password: z
+    .string()
+    .min(10, "Password should be at least 10 characters long")
+    .regex(/\d/, "Password should contain at least one number (0123456789)"),
+});
+
 export async function handleForm(prevState: any, formData: FormData) {
   const password = formData.get("password");
 

8c82e9d200366047de33fbf42654144c0f420ea0
Oh Jisu <parangwavetraveller@gmail.com>
2024-08-26 00:59:26 +0900

    feat: Add PrevButton component

---
+++ components/PrevButton.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { ArrowLeftIcon } from "@heroicons/react/24/solid";
+
+interface ButtonProps {
+  children?: React.ReactNode;
+  className?: string;
+}
+
+export default function PrevButton({
+  children = <ArrowLeftIcon className="size-28" />,
+  className,
+}: ButtonProps) {
+  const handleClick = () => {
+    window.history.back();
+  };
+
+  return (
+    <button
+      onClick={handleClick}
+      className={`flex items-center justify-start ${className}`}
+    >
+      {children}
+    </button>
+  );
+}

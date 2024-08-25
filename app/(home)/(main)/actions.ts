adb4dee20db694778b59254851475657b2fb9ae5
Oh Jisu <parangwavetraveller@gmail.com>
2024-08-26 03:32:34 +0900

    feat: Add action for main page

---
+++ app/(home)/(main)/actions.ts
@@ -0,0 +1,119 @@
+"use server";
+
+import db from "@/lib/db";
+
+/* Tweet Item */
+export async function getInitialTweets() {
+  const tweets = await db.tweet.findMany({
+    select: {
+      id: true,
+      tweet: true,
+      created_at: true,
+      user: {
+        select: {
+          username: true,
+          avatar: true,
+        },
+      },
+      _count: {
+        select: {
+          likes: true,
+          responses: true,
+        },
+      },
+    },
+    take: 5,
+    orderBy: {
+      created_at: "desc",
+    },
+  });
+  return tweets;
+}
+
+/* Tweet Item */
+export async function getTweets(page: number) {
+  const tweets = await db.tweet.findMany({
+    select: {
+      id: true,
+      tweet: true,
+      created_at: true,
+      user: {
+        select: {
+          username: true,
+        },
+      },
+      _count: {
+        select: {
+          likes: true,
+          responses: true,
+        },
+      },
+    },
+    take: 3,
+    skip: page * 3,
+    orderBy: {
+      created_at: "desc",
+    },
+  });
+  return tweets;
+}
+
+export async function getTweetMore(page: number) {
+  const tweets = await db.tweet.findMany({
+    select: {
+      id: true,
+      tweet: true,
+      created_at: true,
+      user: {
+        select: {
+          username: true,
+        },
+      },
+      _count: {
+        select: {
+          likes: true,
+          responses: true,
+        },
+      },
+    },
+    take: 3,
+    skip: page * 3,
+    orderBy: {
+      created_at: "desc",
+    },
+  });
+  return tweets;
+}
+
+/* Tweet Detail */
+export async function getTweetDetails(id: number) {
+  const tweet = await db.tweet.findUnique({
+    where: {
+      id,
+    },
+    select: {
+      id: true,
+      tweet: true,
+      created_at: true,
+      updated_at: true,
+      user: {
+        select: {
+          username: true,
+          avatar: true,
+        },
+      },
+      _count: {
+        select: {
+          likes: true,
+          responses: true,
+        },
+      },
+      likes: {
+        select: {
+          user: true,
+        },
+      },
+    },
+  });
+  return tweet;
+}

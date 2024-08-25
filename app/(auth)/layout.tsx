a5775abd31bf4997772d1c79d0bc34693e5869b8
Oh Jisu <parangwavetraveller@gmail.com>
2024-08-25 23:20:44 +0900

    feat: Add layout in (auth)

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+export default function TabLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="wrapper">
+      <div className="device bg-white">
+        <section className="device-inner">
+          <div className="flex flex-col h-full p-4">{children}</div>
+        </section>
+      </div>
+    </div>
+  );
+}

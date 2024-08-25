c9f63ddbddd49a52b367c80d71e925b87491de59
Oh Jisu <parangwavetraveller@gmail.com>
2024-08-26 01:03:44 +0900

    feat: Modify Tweet component

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,20 +1,20 @@
-import { formatToDateTime } from "@/lib/utils"
-import { RiChat1Line } from "@remixicon/react"
-import { TweetProps } from "@/types"
-import UserAvatar from "../common/avatar"
-import ButtonLike from "../button/button-like"
+import { formatToDateTime } from "@/lib/utils";
+import { TweetProps } from "@/lib/types";
+import { ChatBubbleOvalLeftIcon } from "@heroicons/react/24/outline";
+import Avatar from "./Avatar";
+import LikeButton from "./LikeButton";
 
 export default function TweetItem({
   tweet,
   id,
   user,
   created_at,
-  _count: { likes, comments },
+  _count: { likes, responses },
 }: TweetProps) {
   return (
     <section className="flex flex-col">
       <div className="flex items-center gap-2 p-4">
-        <UserAvatar
+        <Avatar
           width={32}
           height={32}
           src={user.avatar ?? null}
@@ -24,7 +24,7 @@ export default function TweetItem({
 
         <div className="flex flex-col">
           <span className="text-sm">{user.username}</span>
-          <time className="text-xs text-zinc-400">
+          <time className="text-xs text-slate-400">
             {formatToDateTime(created_at.toString())}
           </time>
         </div>
@@ -32,17 +32,17 @@ export default function TweetItem({
 
       <div className="break-words px-4">{tweet}</div>
 
-      <div className="mt-4 flex items-center justify-between gap-4 border-t border-t-zinc-100 px-4 py-3">
+      <div className="mt-4 flex items-center justify-between gap-4 border-t border-t-slate-100 px-4 py-3">
         <span className="flex items-center justify-center gap-4">
           <span className="flex items-center gap-1">
-            <ButtonLike isLiked={false} likeCount={likes} tweetId={id} />
+            <LikeButton isLiked={false} likeCount={likes} tweetId={id} />
           </span>
           <span className="flex items-center gap-1">
-            <RiChat1Line className="size-5 text-zinc-400" />
-            <span className="text-xs text-zinc-400">{comments}</span>
+            <ChatBubbleOvalLeftIcon className="size-5 text-slate-400" />
+            <span className="text-xs text-slate-400">{responses}</span>
           </span>
         </span>
       </div>
     </section>
-  )
+  );
 }

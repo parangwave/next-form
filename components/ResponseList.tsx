2f0fdd1dd18bd70af22b4d1e10d281c1c4ffcbe7
Oh Jisu <parangwavetraveller@gmail.com>
2024-08-26 00:39:13 +0900

    feat: Add ResponseList component

---
+++ components/ResponseList.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { deleteResponse } from "@/app/(home)/(tabs)/tweets/[id]/actions";
+import { Response } from "./Response";
+import { ResponseProps } from "@/lib/types";
+
+export default function responseList({
+  userId,
+  responses,
+}: {
+  id: number;
+  userId: number;
+  responses: ResponseProps[];
+}) {
+  return (
+    <>
+      <ul className="flex flex-col overflow-y-auto border-t-8 border-t-slate-200">
+        {responses.length === 0 ? (
+          <li className="p-4 text-slate-400">댓글을 한 번 남겨볼까요?</li>
+        ) : (
+          responses.map((response) => (
+            <Response
+              key={response.id}
+              id={response.id}
+              user={response.user}
+              created_at={response.created_at}
+              response={response.payload}
+              userId={userId}
+              responseDelete={deleteResponse}
+            />
+          ))
+        )}
+      </ul>
+    </>
+  );
+}

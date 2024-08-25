52b99e9f6e6960206b07ee688e5bc7059085aa05
Oh Jisu <parangwavetraveller@gmail.com>
2024-08-26 03:34:43 +0900

    Deisgn: Redesign page for tweets/add

--- app/(home)/(tabs)/tweets/add/page.tsx
+++ app/(home)/(tabs)/tweets/add/page.tsx
@@ -8,7 +8,7 @@ import Button from "@/components/Button";
 export default function Home() {
   const [state, action] = useFormState(handleForm, null);
   return (
-    <div className="w-full h-screen flex flex-col justify-center items-center gap-3 ">
+    <div className="w-full h-full flex flex-col justify-center items-center gap-3 ">
       <form className="flex flex-col gap-3 w-1/4" action={action}>
         <h1 className="self-center">create tweet</h1>
         <Input

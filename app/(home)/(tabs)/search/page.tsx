9262dea2cd44d4e9c8d54fef2870a36e261bda6c
Oh Jisu <parangwavetraveller@gmail.com>
2024-08-26 03:33:36 +0900

    feat: Add page for search page

---
+++ app/(home)/(tabs)/search/page.tsx
@@ -0,0 +1,11 @@
+import SearchInput from "@/components/SearchInput";
+
+export default function SearchPage() {
+  return (
+    <>
+      <div className="px-4">
+        <SearchInput />
+      </div>
+    </>
+  );
+}

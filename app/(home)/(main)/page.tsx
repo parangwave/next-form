84c6539090bc37281bb7ce35bb8672e08b8e69cf
Oh Jisu <parangwavetraveller@gmail.com>
2024-08-26 03:32:42 +0900

    feat: Add page for main page

---
+++ app/(home)/(main)/page.tsx
@@ -0,0 +1,18 @@
+import TweetList from "@/components/TweetList";
+import { Prisma } from "@prisma/client";
+import { getInitialTweets } from "./actions";
+
+export type InitialTweets = Prisma.PromiseReturnType<typeof getInitialTweets>;
+
+export const metadata = {
+  title: "홈",
+};
+
+export default async function HomePage() {
+  const initialTweets = await getInitialTweets();
+  return (
+    <>
+      <TweetList initialTweets={initialTweets} />
+    </>
+  );
+}

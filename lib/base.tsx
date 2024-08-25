09bc66e71826165510780b168e5cf8dcb4562456
Oh Jisu <parangwavetraveller@gmail.com>
2024-08-26 03:28:35 +0900

    feat: Resize icon for TabBar

--- lib/base.tsx
+++ lib/base.tsx
@@ -76,40 +76,40 @@ export const baseTabItems: ITabPath[] = [
     label: "트윗",
     href: "/",
     icon: {
-      active: <HomeIconFill className="text-slate-700" />,
-      inactive: <HomeIconLine />,
+      active: <HomeIconFill className="size-12 text-slate-700" />,
+      inactive: <HomeIconLine className="size-12" />,
     },
   },
   {
     label: "검색",
     href: "/search",
     icon: {
-      active: <SearchIconFill className="size-20 text-slate-700" />,
-      inactive: <SearchIconLine className="size-20" />,
+      active: <SearchIconFill className="size-12 text-slate-700" />,
+      inactive: <SearchIconLine className="size-12" />,
     },
   },
   {
     label: "추가",
     href: "/tweets/add",
     icon: {
-      active: <PlusCircleIconFill className="size-20 text-slate-700" />,
-      inactive: <PlusCircleIconLine className="size-20" />,
+      active: <PlusCircleIconFill className="size-12 text-slate-700" />,
+      inactive: <PlusCircleIconLine className="size-12" />,
     },
   },
   {
     label: "좋아요",
     href: "/likes",
     icon: {
-      active: <HeartIconFill className="size-20 text-slate-700" />,
-      inactive: <HeartIconLine />,
+      active: <HeartIconFill className="size-12 text-slate-700" />,
+      inactive: <HeartIconLine className="size-12" />,
     },
   },
   {
     label: "프로필",
     href: "/users/:id",
     icon: {
-      active: <UserIconFill className="size-20 text-slate-700" />,
-      inactive: <UserIconLine />,
+      active: <UserIconFill className="size-12 text-slate-700" />,
+      inactive: <UserIconLine className="size-12" />,
     },
   },
 ];

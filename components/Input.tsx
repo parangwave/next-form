15513863c36be858310636dde8c61c340430ef1a
Oh Jisu <parangwavetraveller@gmail.com>
2024-08-26 01:11:46 +0900

    feat: Modify Input component

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,27 +1,60 @@
-import { InputHTMLAttributes } from "react";
+import {
+  ForwardedRef,
+  InputHTMLAttributes,
+  forwardRef,
+  ReactNode,
+} from "react";
 
 interface InputProps {
+  type?: string;
+  placeholder?: string;
+  required?: boolean;
   name: string;
   errors?: string[];
+  icon?: ReactNode;
+  className?: string;
 }
 
-export default function Input({
-  name,
-  errors = [],
-  ...rest
-}: InputProps & InputHTMLAttributes<HTMLInputElement>) {
+const _Input = (
+  {
+    type = "text",
+    placeholder,
+    required,
+    name,
+    errors = [],
+    icon,
+    className,
+    ...rest
+  }: InputProps & InputHTMLAttributes<HTMLInputElement>,
+  ref: ForwardedRef<HTMLInputElement>
+) => {
+  const hasError = errors && errors.length > 0;
   return (
-    <div className="flex flex-col gap-2">
-      <input
-        name={name}
-        className="px-3 bg-transparent rounded-md w-full h-10 focus:outline-none ring-2 focus:ring-4 transition ring-neutral-200 focus:ring-orange-500 border-none placeholder:text-neutral-400"
-        {...rest}
-      />
+    <>
+      <label
+        className={`relative flex items-center gap-4 my-1 mt-3 ${className}`}
+      >
+        {icon}
+        <input
+          ref={ref}
+          name={name}
+          type={type}
+          className={`grow bg-slate-50 h-12 pl-10 focus:ring-2 focus:ring-rose ${
+            hasError ? "outline outline-2 outline-red-500" : ""
+          }`}
+          placeholder={placeholder}
+          required={required}
+          {...rest}
+        />
+      </label>
+
       {errors.map((error, index) => (
         <span key={index} className="text-red-500 font-medium">
-          {error}
+          * {error}
         </span>
       ))}
-    </div>
+    </>
   );
-}
+};
+
+export default forwardRef(_Input);

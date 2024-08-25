410fccc1fc64f197af05161df13e5dde79dc5289
Oh Jisu <parangwavetraveller@gmail.com>
2024-08-26 00:48:17 +0900

    feat: Add types for user, Tweet, like

--- lib/types.ts
+++ lib/types.ts
@@ -1,3 +1,45 @@
+export interface UserProps {
+  id: number;
+  username: string;
+  password: string | null;
+  email: string | null;
+  phone: string | null;
+  bio: string | null;
+  avatar: string | null;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export interface TweetProps {
+  id: number;
+  tweet: string;
+  created_at: Date;
+  updated_at?: Date;
+  user: {
+    username: string;
+    avatar: string | null;
+  };
+  _count: {
+    likes: number;
+    responses: number;
+  };
+}
+
+export interface LikeProps {
+  user: {
+    id: number;
+    username: string;
+    avatar?: string;
+  };
+  tweet: {
+    id: number;
+    created_at: Date;
+    updated_at: Date;
+  };
+  created_at: Date;
+  updated_at: Date;
+}
+
 export interface ResponseProps {
   user: {
     id: number;
@@ -11,5 +53,4 @@ export interface ResponseProps {
   id: number;
   created_at: Date;
   updated_at: Date;
-  payload: string;
 }

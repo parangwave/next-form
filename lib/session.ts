81d6722b95ff8be46061962d42b5f6fea8802536
Oh Jisu <parangwavetraveller@gmail.com>
2024-08-26 05:22:02 +0900

    feat: Fix session

--- lib/session.ts
+++ lib/session.ts
@@ -6,16 +6,8 @@ interface SessionContent {
 }
 
 export default function getSession() {
-  const sessionPassword = process.env.COOKIE_PASSWORD;
-
-  if (!sessionPassword || sessionPassword.length < 32) {
-    throw new Error(
-      "The session password is either not set or is too short. It must be at least 32 characters long."
-    );
-  }
-
   return getIronSession<SessionContent>(cookies(), {
     cookieName: "tweet-cookies",
-    password: sessionPassword,
+    password: process.env.COOKIE_PASSWORD!,
   });
 }
